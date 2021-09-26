b8b9ce36b9d0083f1bdc7dd148bfa71917f47ab6
mahendruankit <ankit.mahendru@gmail.com>
2021-09-26 17:09:02 +0100

    add mark done styling and add todo item func

--- Index.js
+++ Index.js
@@ -1,5 +1,4 @@
 let taskLists = [];
-let todoLists = [];
 
 /** TaskList Popup **/
 
@@ -27,6 +26,7 @@ function addTaskList(heading) {
   const taskList = {
     id: 'tasklist' + Date.now(),
     heading: heading,
+    todoList: [],
   };
 
   taskLists.push(taskList);
@@ -46,69 +46,96 @@ function deleteTaskList(id) {
 
 function renderTaskList(taskLists) {
   var cards = document.getElementById('cards');
-
+  var message = document.getElementById('message');
   //Remove all child nodes
   cards.innerHTML = '';
 
   var frag = document.createDocumentFragment();
 
+  //No items in the todo list
   if (taskLists.length === 0) {
-    cards.remove();
+    message.classList.remove('message--inactive');
+    cards.innerHTML = '';
+  } else {
+    taskLists.forEach((taskList) => {
+      message.classList.add('message--inactive');
+
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+      card.setAttribute('data-key', taskList.id);
+
+      const cardDetails = document.createElement('div');
+      cardDetails.setAttribute('class', 'card-details');
+
+      const cardTitle = document.createElement('div');
+      cardTitle.setAttribute('class', 'card-title');
+      cardTitle.innerHTML = taskList.heading;
+      //append card title
+      cardDetails.appendChild(cardTitle);
+
+      //append todo if exists
+      if (taskList.todoList.length > 0) {
+        const ul = document.createElement('ul');
+        ul.setAttribute('class', 'card-todo-list');
+
+        taskList.todoList.forEach((todo) => {
+          const li = document.createElement('li');
+          li.setAttribute('class', 'card-todo-item');
+
+          const todoButton = document.createElement('BUTTON');
+          todoButton.setAttribute('class', 'card-todo-button');
+          todoButton.innerHTML = 'Mark done';
+
+          li.innerHTML = todo.heading;
+
+          li.appendChild(todoButton);
+          ul.appendChild(li);
+        });
+
+        cardDetails.appendChild(ul);
+      }
+
+      const cardDeleteIcon = document.createElement('i');
+      cardDeleteIcon.setAttribute('class', 'fas fa-minus-circle fa-2x');
+
+      const cardDeleteContainer = document.createElement('div');
+      cardDeleteContainer.setAttribute('class', 'card-delete');
+      cardDeleteContainer.setAttribute(
+        'onclick',
+        'deleteTaskList( "' + taskList.id + '" )'
+      );
+
+      cardDeleteContainer.appendChild(cardDeleteIcon);
+
+      const cardAddIcon = document.createElement('i');
+      cardAddIcon.setAttribute('class', 'fas fa-plus-circle fa-2x');
+
+      const cardAddContainer = document.createElement('div');
+      cardAddContainer.setAttribute('class', 'card-add');
+      cardAddContainer.setAttribute(
+        'onclick',
+        'loadTodoPopup( "' + taskList.id + '" )'
+      );
+
+      cardAddContainer.appendChild(cardAddIcon);
+
+      //append card details element
+
+      cardDetails.appendChild(cardDeleteContainer);
+      cardDetails.appendChild(cardAddContainer);
+
+      //appened card element
+
+      card.appendChild(cardDetails);
+
+      //append fragment
+
+      frag.appendChild(card);
+
+      //append cards element
+      cards.appendChild(frag);
+    });
   }
-
-  taskLists.forEach((taskList) => {
-    const card = document.createElement('div');
-    card.setAttribute('class', 'card');
-    card.setAttribute('data-key', taskList.id);
-
-    const cardDetails = document.createElement('div');
-    cardDetails.setAttribute('class', 'card-details');
-
-    const cardTitle = document.createElement('div');
-    cardTitle.setAttribute('class', 'card-title');
-    cardTitle.innerHTML = taskList.heading;
-
-    const cardDeleteIcon = document.createElement('i');
-    cardDeleteIcon.setAttribute('class', 'fas fa-minus-circle fa-2x');
-
-    const cardDeleteContainer = document.createElement('div');
-    cardDeleteContainer.setAttribute('class', 'card-delete');
-    cardDeleteContainer.setAttribute(
-      'onclick',
-      'deleteTaskList( "' + taskList.id + '" )'
-    );
-
-    cardDeleteContainer.appendChild(cardDeleteIcon);
-
-    const cardAddIcon = document.createElement('i');
-    cardAddIcon.setAttribute('class', 'fas fa-plus-circle fa-2x');
-
-    const cardAddContainer = document.createElement('div');
-    cardAddContainer.setAttribute('class', 'card-add');
-    cardAddContainer.setAttribute(
-      'onclick',
-      'loadTodoPopup( "' + taskList.id + '" )'
-    );
-
-    cardAddContainer.appendChild(cardAddIcon);
-
-    //Append Card Details Element
-
-    cardDetails.appendChild(cardTitle);
-    cardDetails.appendChild(cardDeleteContainer);
-    cardDetails.appendChild(cardAddContainer);
-
-    //Append Card Element
-
-    card.appendChild(cardDetails);
-
-    //Append Fragment
-
-    frag.appendChild(card);
-
-    //Append Cards Element
-    cards.appendChild(frag);
-  });
 }
 
 /** Todo Popup **/
@@ -130,15 +157,26 @@ function loadTodoPopup(id) {
 function closeTodoPopup() {
   var popupContainer = document.getElementById('todoPopup');
   var page = document.getElementById('page');
-  var listName = document.getElementById('listName');
 
   page.style.filter = '';
   popupContainer.style.display = 'none';
-  listName.value = '';
 }
 
 /** To do **/
 
 function addTodoItem(id) {
-  console.log('add to do item function' + id);
+  const todo = {
+    id: 'todo' + Date.now(),
+    heading: document.getElementById('todoName').value,
+  };
+
+  for (let i = 0; i < taskLists.length; i++) {
+    if (taskLists[i].id == id) {
+      var todoList = taskLists[i].todoList;
+      todoList.push(todo);
+      console.log(todoList);
+    }
+  }
+  closeTodoPopup();
+  renderTaskList(taskLists);
 }
